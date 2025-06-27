4958820fa02a9207410b34ecf5a573abba7f3a5f
Leander <leander-arun.ziehm@stud.th-deg.de>
2025-06-27 17:05:52 +0200

    v1.5

--- 1 copy.py
+++
@@ -1,8 +0,0 @@
-from email_code import get_recent_emails
-from application_code import get_users_name
-from llm_code import get_llm_response
-
-emails = get_recent_emails("my-email@gmail.com")
-name = get_users_name() #"Leander"
-prompt = f"You are a email assistant. Here are {name}'s recent emails {emails}"
-llm_response = get_llm_response(prompt)
