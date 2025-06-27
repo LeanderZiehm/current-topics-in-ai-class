aeb1b6afae1be524da0e83f90fe9397aff755007
Leander <leander-arun.ziehm@stud.th-deg.de>
2025-06-27 22:56:52 +0200

    v2

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+# from home import home_bp  # Import the blueprint
+
+app = Flask(__name__)
+
+# Register the blueprint
+# app.register_blueprint(home_bp)
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/prompt-template-system')
+def prompt_template_system():
+    return render_template("prompt-template-system.html")
+
+@app.route('/prompt-tracking')
+def prompt_tracking():
+    return render_template("prompt-tracking.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
